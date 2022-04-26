51a9be0c191f823197a2ee7820b29a291b155edf
Salumao Barbosa da Costa <salubcosta@gmail.com>
2022-04-26 16:54:43 -0300

    brincadeira com strings

---
+++ operadores/concatenacao-strings.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  var nameSchool = "UNITPAC";
+  var nameCourse = "Information System";
+  var startYear = 2009;
+  var endYear = 2013;
+
+  print(nameSchool + " - " + nameCourse);
+  print("Start course: " + startYear.toString());
+  print("End course: " + endYear.toString());
+  print("Elapsed time: " + (endYear - startYear).toString() + " years.");
+}
