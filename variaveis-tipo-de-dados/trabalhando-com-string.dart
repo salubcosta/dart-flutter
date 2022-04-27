cb8cdde0358c9479729a6e4d9bcc1ecdbc700e08
Salumao Barbosa da Costa <salubcosta@gmail.com>
2022-04-27 17:11:17 -0300

    brincando com strings

---
+++ variaveis-tipo-de-dados/trabalhando-com-string.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  String fullName = 'Salumão Barbosa';
+
+  print(fullName.length);
+  print(fullName.toUpperCase());
+  print(fullName.toLowerCase());
+  print(fullName.isEmpty);
+  print(fullName.isNotEmpty);
+  print(fullName.codeUnitAt(14));
+}
