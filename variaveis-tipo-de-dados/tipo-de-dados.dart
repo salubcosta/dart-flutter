65aa8f3ef58bd42914c67cf5f92c0654079d0896
Salumao Barbosa da Costa <salubcosta@gmail.com>
2022-04-27 17:10:23 -0300

    tipo de dados

---
+++ variaveis-tipo-de-dados/tipo-de-dados.dart
@@ -0,0 +1,14 @@
+void main() {
+  String name = "Salumao";
+
+  int age = 30;
+
+  double wage = 3010.9;
+
+  bool isMarried = true;
+
+  dynamic severalTypes = "";
+  severalTypes = false;
+
+  print(wage);
+}
