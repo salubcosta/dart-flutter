26d071937833d8d28e76f6570121d62956388a79
Salumao Barbosa da Costa <salubcosta@gmail.com>
2022-04-27 16:07:04 -0300

    operadores relacionais

---
+++ operadores/operadores-relacionais.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  int number1 = 5;
+  int number2 = 10;
+
+  print(number1 == number2);
+  print(number1 != number2);
+  print(number1 > number2);
+  print(number1 < number2);
+  print(number1 >= number2);
+  print(number1 <= number2);
+}
